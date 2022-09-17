190be197e41182db79e7efd613ff1639ee41180a
Aleezah Ikram <aleezahikram1429@gmail.com>
2022-09-17 23:05:04 +0500

    Become A Donor UI done 9/17/2022

--- src/container/BecomeADonor.js
+++ src/container/BecomeADonor.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
-import { View, Text, Button, TextInput,ScrollView } from 'react-native';
+import {
+  View, Text, Button, TextInput, ScrollView, Modal, StyleSheet, TouchableOpacity, Image, Pressable,
+  StatusBar
+} from 'react-native';
 import database from "@react-native-firebase/database";
-import { useState,useEffect } from 'react';
-import {Picker} from "@react-native-picker/picker";
+import { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ToastAndroid } from 'react-native';
+import { ImageBackground } from 'react-native';
+
 
 
 function BecomeDonor(props) {
@@ -16,15 +20,18 @@ function BecomeDonor(props) {
   const [health, setHealth] = useState("");
   const [address, setAddress] = useState("");
 
-console.log(props.logerDonor)
-  const save_data=()=>{
+  const [modalVisible, setModalVisible] = useState(false);
 
-    let donor={
-        name, gender , email , phone , address , blood , health
+
+  console.log(props.logerDonor)
+  const save_data = () => {
+
+    let donor = {
+      name, gender, email, phone, address, blood, health
     }
-    
-    
-    database().ref(`/donors/${donor.name}`).update({donor})
+
+
+    database().ref(`/donors/${donor.name}`).update({ donor })
 
     const storeData = async (donor) => {
       try {
@@ -35,66 +42,251 @@ console.log(props.logerDonor)
       }
     }
     storeData(donor);
-    ToastAndroid.show("You are now a donor",ToastAndroid.SHORT)
+    ToastAndroid.show("You are now a donor", ToastAndroid.SHORT)
     props.navigation.navigate("Profile");
   }
 
-  
-  
-// console.log("xkbsxb",props.logerDonor)
-  
+
+
   return (
-<ScrollView>
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <View>
-        <Text style={{ fontSize: 50, color: 'red', fontWeight: 'bold',marginBottom:10,textAlign:"center",marginTop:50 }}>Become Donor</Text>
-        <Text style={{ fontSize: 20, color: 'red',marginBottom:10,textAlign:"center" }}> {(props.logerDonor!="")?"You are already a donor. Your donor information will be updated":""}</Text>
+    <>
+      <View style={styles.container}>
+        <StatusBar backgroundColor='red' barStyle="light-content" />
+        <View style={styles.header}>
+          <Text style={[styles.text_header, { textAlign: "center" }]}>BECOME A DONOR</Text>
+        </View>
+        <View
+          style={styles.footer}
+        >
+          <ImageBackground source={require("../images/bgBecomeADonor.png")} style={{width: "100%",}}>
+            <ScrollView>
+
+              <Text style={{ fontSize: 18, color: 'red', marginBottom: 10, textAlign: "center" }}> {(props.logerDonor != "") ? "You are already a donor. Your donor information will be updated" : ""}</Text>
+
+              <View style={styles.inputStyle}>
+                <TextInput value={name} onChangeText={(e) => setName(e)} placeholder="Name" />
+              </View>
+              <View style={styles.inputStyle}>
+                <TextInput value={gender} onChangeText={(e) => setGender(e)} placeholder="Gender" />
+              </View>
+              <View style={styles.inputStyle}>
+                <TextInput value={email} onChangeText={(e) => setEmail(e)} placeholder="Email" />
+              </View>
+              <View style={styles.inputStyle}>
+                <TextInput keyboardType={"number-pad"} value={phone} onChangeText={(e) => setPhone(e)} placeholder="Phone Number" />
+              </View>
+              <View style={styles.inputStyle}>
+                <TextInput value={address} onChangeText={(e) => setAddress(e)} placeholder="Address" />
+              </View>
+              <View style={styles.inputStyle}>
+                <TextInput value={health} onChangeText={(e) => setHealth(e)} placeholder="Health" />
+              </View>
+
+              <View style={styles.inputStyle}>
+                <Button color="red" onPress={() => setModalVisible(!modalVisible)} title={blood == "" ? "Select Blood Type" : blood}></Button>
+              </View>
+
+              <View>
+                <View style={{ marginTop: 20, width: 150, marginLeft: 5, marginBottom: 20 }}>
+                  <Button color="red" style={{ fontSize: 30 }} onPress={() => save_data()} title="Save"></Button>
+                </View>
+              </View>
+
+              {/* </View> */}
+
+            </ScrollView>
+          </ImageBackground>
+        </View>
       </View>
-      <View style={{ borderWidth: 3, borderColor: "red", width: "80%", margin: 5 }}>
-        <TextInput  value={name} onChangeText={(e) => setName(e)} placeholder="Name" />
-      </View>
-       <View style={{ borderWidth: 3, borderColor: "red", width: "80%", margin: 5 }}>
-        <TextInput  value={gender} onChangeText={(e) => setGender(e)} placeholder="Gender" />
-      </View>
-      <View style={{borderWidth:3,borderColor:"red",width:"80%", margin:5}}>
-        <TextInput value={email} onChangeText={(e)=>setEmail(e)} placeholder="Email"/>
-      </View>
-      <View style={{borderWidth:3,borderColor:"red",width:"80%", margin:5}}>
-        <TextInput keyboardType={"number-pad"}  value={phone} onChangeText={(e)=>setPhone(e)} placeholder="Phone Number"/>
-      </View>
-      <View style={{borderWidth:3,borderColor:"red",width:"80%", margin:5}}>
-        <TextInput value={address} onChangeText={(e)=>setAddress(e)} placeholder="Address"/>
-      </View>
-      <View style={{borderWidth:3,borderColor:"red",width:"80%", margin:5}}>
-        <TextInput value={health} onChangeText={(e)=>setHealth(e)} placeholder="Health"/>
-      </View>
-      
-      <View style={{borderWidth:3,borderColor:"red"}}>
-      <Picker
-        selectedValue={blood}
-        style={{ height: 50, width: 150 }}
-        onValueChange={(itemValue) => setBlood(itemValue)}>
-        <Picker.Item label="A+" value="A+" />
-        <Picker.Item label="A-" value="A-" />
-        <Picker.Item label="B+" value="B-" />
-        <Picker.Item label="B-" value="B-" />
-        <Picker.Item label="O+" value="O+" />
-        <Picker.Item label="O-" value="O-" />
-        <Picker.Item label="AB+" value="AB+" />
-        <Picker.Item label="AB-" value="AB-" />
-      </Picker>
-      </View>
-    <View>
-      <View style={{marginTop:20, width:150}}>
-      <Button  color = "red" style={{fontSize:30}} onPress={()=>save_data()} title="Save"></Button>
-      </View>
-    </View>
-    
-    </View>
-    </ScrollView>
+
+
+
+
+
+
+      {/* Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View style={{ flexDirection: "row" }}>
+              <TouchableOpacity onPress={() => setBloodType("A+")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/A+.jpeg")} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setBloodType("B+")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/B+.jpeg")} />
+              </TouchableOpacity>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <TouchableOpacity onPress={() => setBloodType("AB+")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/AB+.jpeg")} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setBloodType("O+")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/O+.jpeg")} />
+              </TouchableOpacity>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <TouchableOpacity onPress={() => setBloodType("A-")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/A-.jpeg")} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setBloodType("B-")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/B-.jpeg")} />
+              </TouchableOpacity>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <TouchableOpacity onPress={() => setBloodType("AB-")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/AB-.jpeg")} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setBloodType("O-")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/O-.jpeg")} />
+              </TouchableOpacity>
+            </View>
+
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 }
 
+const styles = StyleSheet.create({
+  inputStyle: {
+    borderWidth: 2,
+    borderColor: "red",
+    width: "95%",
+    margin: 5,
+    borderRadius: 5
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2
+  },
+  buttonClose: {
+    backgroundColor: "red",
+    marginTop: 20
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  opacityType: {
+    width: "33%", margin: 15, borderRadius: 10, borderColor: "red", borderWidth: 1, shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.39,
+    shadowRadius: 8.30,
 
+    elevation: 13,
+  },
+  BloodType: {
+    height: 50, width: "90%", borderRadius: 10, margin: 5
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+    textAlign: "center"
+  },
+  footer: {
+    flex: Platform.OS === 'ios' ? 3 : 5,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30
+  },
+  text_header: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+  text_footer: {
+    color: '#1f1f14',
+    fontSize: 18
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: '#05375a',
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 20
+  },
+  signIn: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  textPrivate: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 20
+  },
+  color_textPrivate: {
+    color: 'grey'
+  }
+});
 
 export default BecomeDonor;
--- src/container/signUp.js
+++ src/container/signUp.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { View, Text, Button, TextInput, Image,Modal,Pressable,StyleSheet,TouchableOpacity } from 'react-native';
+import { View, Text, Button, TextInput, Image, Modal, Pressable, StyleSheet, TouchableOpacity } from 'react-native';
 import database from "@react-native-firebase/database";
 import { useState } from 'react';
-import {Picker} from "@react-native-picker/picker";
 
 
 function SignUp(props) {
@@ -12,12 +11,12 @@ function SignUp(props) {
   const [pass, setPass] = useState("");
   const [phone, setPhone] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
-  const save_data=()=>{
-    let user={
-        name, email , pass , phone, blood
+  const save_data = () => {
+    let user = {
+      name, email, pass, phone, blood
     }
     var id = email.split("@")
-    database().ref(`/users/${id[0]}`).update({name, email , pass , phone, blood});
+    database().ref(`/users/${id[0]}`).update({ name, email, pass, phone, blood });
     props.navigation.navigate("Login");
     setName("")
     setEmail("")
@@ -28,59 +27,44 @@ function SignUp(props) {
   }
 
 
-  const setBloodType=(blood)=>{
-      setblood(blood)
-      setModalVisible(false)
+  const setBloodType = (blood) => {
+    setblood(blood)
+    setModalVisible(false)
   }
 
   return (
-<>
-    <View  style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'white' }}>
-      <View style={{backgroundColor: "red",width:"90%", margin:20,borderTopLeftRadius:60,borderBottomRightRadius:60,padding:5,marginBottom:40}}>
-        <Text style={{ fontSize: 45, color: 'white', fontWeight: 'bold', textAlign:'center' }}>SIGNUP</Text>
+    <>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
+        <View style={{ backgroundColor: "red", width: "90%", margin: 20, borderTopLeftRadius: 60, borderBottomRightRadius: 60, padding: 5, marginBottom: 40 }}>
+          <Text style={{ fontSize: 45, color: 'white', fontWeight: 'bold', textAlign: 'center' }}>SIGNUP</Text>
+        </View>
+        <View style={{ borderWidth: 3, borderColor: "red", width: "80%", margin: 5 }}>
+          <TextInput value={name} onChangeText={(e) => setName(e)} placeholder="Name" />
+        </View>
+        <View style={{ borderWidth: 3, borderColor: "red", width: "80%", margin: 5 }}>
+          <TextInput value={email} onChangeText={(e) => setEmail(e)} placeholder="Email" />
+        </View>
+        <View style={{ borderWidth: 3, borderColor: "red", width: "80%", margin: 5 }}>
+          <TextInput secureTextEntry={true} value={pass} onChangeText={(e) => setPass(e)} placeholder="Password" />
+        </View>
+        <View style={{ borderWidth: 3, borderColor: "red", width: "80%", margin: 5 }}>
+          <TextInput value={phone} onChangeText={(e) => setPhone(e)} placeholder="Contact Number" />
+        </View>
+        <View style={{ borderWidth: 3, borderColor: "red", width: "80%", margin: 5 }}>
+          <Button color="red" onPress={() => setModalVisible(!modalVisible)} title={blood == "" ? "Select Blood Type" : blood}></Button>
+        </View>
+        <View style={{ margin: 10, width: 150 }}>
+          <Button color="red" onPress={() => save_data()} title="Signup"></Button>
+        </View>
+        <View>
+          <Image source={require('../images/icon.jpg')}
+            style={{ margin: 10, height: 150, width: 200 }}
+          />
+        </View>
       </View>
-      <View style={{ borderWidth: 3, borderColor: "red", width: "80%", margin: 5 }}>
-        <TextInput value={name} onChangeText={(e) => setName(e)} placeholder="Name" />
-      </View>
-      <View style={{borderWidth:3,borderColor:"red",width:"80%", margin:5}}>
-        <TextInput value={email} onChangeText={(e)=>setEmail(e)} placeholder="Email"/>
-      </View>
-      <View style={{ borderWidth: 3, borderColor: "red", width: "80%", margin: 5 }}>
-        <TextInput secureTextEntry={true} value={pass} onChangeText={(e) => setPass(e)} placeholder="Password" />
-      </View>
-      <View style={{borderWidth:3,borderColor:"red",width:"80%", margin:5}}>
-        <TextInput value={phone} onChangeText={(e)=>setPhone(e)} placeholder="Contact Number"/>
-      </View>
-      <View  style={{borderWidth:3,borderColor:"red",width:"80%", margin:5}}>
-      <Button color = "red" onPress={()=>setModalVisible(!modalVisible)} title={blood==""?"Select Blood Type":blood}></Button>
 
-      {/* <Picker
-        selectedValue={blood}
-        style={{ height: 50, width: 280,}}
-        onValueChange={(itemValue) => setblood(itemValue)}
-      >
-      <Picker.Item label="A+" value="A+" />
-        <Picker.Item label="A-" value="A-" />
-        <Picker.Item label="B+" value="B-" />
-        <Picker.Item label="B-" value="B-" />
-        <Picker.Item label="O+" value="O+" />
-        <Picker.Item label="O-" value="O-" />
-        <Picker.Item label="AB+" value="AB+" />
-        <Picker.Item label="AB-" value="AB-" />
-      </Picker> */}
-    </View>
-      <View style={{margin:10, width:150}}>
-      <Button color = "red" onPress={()=>save_data()} title="Signup"></Button>
-      </View>
-      <View>
-      <Image source={require('../images/icon.jpg')} 
-      style={{margin:10, height:150, width:200 }}
-      />
-      </View>
-    </View>
-
-                  {/* Modal */}
-<Modal
+      {/* Modal */}
+      <Modal
         animationType="slide"
         transparent={true}
         visible={modalVisible}
@@ -91,39 +75,39 @@ function SignUp(props) {
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <View style={{flexDirection:"row"}}>
-              <TouchableOpacity onPress={()=>setBloodType("A+")} style={styles.opacityType}>
-              <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/A+.jpeg")}/>
+            <View style={{ flexDirection: "row" }}>
+              <TouchableOpacity onPress={() => setBloodType("A+")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/A+.jpeg")} />
               </TouchableOpacity>
-              <TouchableOpacity onPress={()=>setBloodType("B+")} style={styles.opacityType}>
-              <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/B+.jpeg")}/>
-              </TouchableOpacity>
-              </View>
-            <View style={{flexDirection:"row"}}>
-              <TouchableOpacity onPress={()=>setBloodType("AB+")} style={styles.opacityType}>
-              <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/AB+.jpeg")}/>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={()=>setBloodType("O+")} style={styles.opacityType}>
-              <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/O+.jpeg")}/>
+              <TouchableOpacity onPress={() => setBloodType("B+")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/B+.jpeg")} />
               </TouchableOpacity>
             </View>
-            <View style={{flexDirection:"row"}}>
-              <TouchableOpacity onPress={()=>setBloodType("A-")} style={styles.opacityType}>
-              <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/A-.jpeg")}/>
+            <View style={{ flexDirection: "row" }}>
+              <TouchableOpacity onPress={() => setBloodType("AB+")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/AB+.jpeg")} />
               </TouchableOpacity>
-              <TouchableOpacity onPress={()=>setBloodType("B-")} style={styles.opacityType}>
-              <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/B-.jpeg")}/>
-              </TouchableOpacity>
-              </View>
-            <View style={{flexDirection:"row"}}>
-              <TouchableOpacity onPress={()=>setBloodType("AB-")} style={styles.opacityType}>
-              <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/AB-.jpeg")}/>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={()=>setBloodType("O-")} style={styles.opacityType}>
-              <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/O-.jpeg")}/>
+              <TouchableOpacity onPress={() => setBloodType("O+")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/O+.jpeg")} />
               </TouchableOpacity>
             </View>
-            
+            <View style={{ flexDirection: "row" }}>
+              <TouchableOpacity onPress={() => setBloodType("A-")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/A-.jpeg")} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setBloodType("B-")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/B-.jpeg")} />
+              </TouchableOpacity>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <TouchableOpacity onPress={() => setBloodType("AB-")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/AB-.jpeg")} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setBloodType("O-")} style={styles.opacityType}>
+                <Image resizeMode='stretch' style={styles.BloodType} source={require("../images/O-.jpeg")} />
+              </TouchableOpacity>
+            </View>
+
             <Pressable
               style={[styles.button, styles.buttonClose]}
               onPress={() => setModalVisible(!modalVisible)}
@@ -133,7 +117,7 @@ function SignUp(props) {
           </View>
         </View>
       </Modal>
-      </>
+    </>
   );
 }
 
@@ -166,7 +150,7 @@ const styles = StyleSheet.create({
   },
   buttonClose: {
     backgroundColor: "red",
-    marginTop:20
+    marginTop: 20
   },
   textStyle: {
     color: "white",
@@ -177,19 +161,19 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     textAlign: "center"
   },
-  opacityType:{
-    width:"33%",margin:15,borderRadius:10,borderColor:"red",borderWidth:1,shadowColor: "#000",
+  opacityType: {
+    width: "33%", margin: 15, borderRadius: 10, borderColor: "red", borderWidth: 1, shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 6,
     },
     shadowOpacity: 0.39,
     shadowRadius: 8.30,
-    
+
     elevation: 13,
   },
-  BloodType:{
-    height:50,width:"90%",borderRadius:10,margin:5
+  BloodType: {
+    height: 50, width: "90%", borderRadius: 10, margin: 5
   }
 });
 
